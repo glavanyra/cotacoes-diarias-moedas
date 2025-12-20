8d95738d3ee65866ff35eebf6593d68df1f7e1b9
vanessa_ysla <glavanyra@gmail.com>
2025-12-20 18:19:31 -0300

    docs: adicionar sistema de logs ao pipeline

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 from src.extract import buscar_cotacao
 from src.transform import transformar_dados
 from src.load import salvar_parquet, upload_to_kaggle # Importe a nova função
+import logging
+
+# Configuração do "Diário de Bordo" (Log)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler() # Isso faz o log aparecer no GitHub Actions
+    ]
+)
 
 def executar_pipeline():
+    logging.info("Iniciando o pipeline de cotações...")
+
     # 1. Extração
-    print("Extraindo dados...")
     dados = buscar_cotacao()
 
-    # Adicinar verificação ("filtro" de segurança)
+    # Adicionar verificação ("filtro" de segurança)
     if dados is None:
-        print("Pipeline encerrado: Não foi possível obter dados da API.")
+        logging.error("Falha na extração: API indisponível ou limite excedido.")
         return
     
     # 2. Transformação
-    print("Transformando dados...")
-    df_final = transformar_dados(dados)
+    try:
+        df_final = transformar_dados(dados)
+        logging.info(f"Transformação concluída. {len(df_final)} linhas processadas.")
+    except Exception as e:
+        logging.error(f"Erro na transformação: {e}")
+        return
     
     # 3. Carga Local (Acumulando dados)
-    print("Salvando localmente...")
+    logging.info("Salvando localmente...")
     salvar_parquet(df_final)
 
     # 4. Upload para Kaggle
-    print("Publicando no Kaggle...")
+    logging.info("Iniciando upload para o Kaggle...")
     try:
         upload_to_kaggle()
+        logging.info("Pipeline finalizado com sucesso!")
     except Exception as e:
         print(f"Erro ao subir para Kaggle: {e}")
 
